764c01c074d3ba0c9db52ff77ec5217d68da6d9c
kinka-jp <lunwu.xiaoxia@gmail.com>
2016-12-18 00:14:03 +0900

    Update websites.java
    deleted by tan.

--- src/web/dao/websites.java
+++ src/web/dao/websites.java
@@ -10,7 +10,7 @@ public class websites {
 	
 	public static void main(String args[]) {
 	    try {
-	      Class.forName("com.mysql.jdbc.Driver");     //加载MYSQL JDBC驱动程序   
+	      Class.forName("com.mysql.jdbc.Driver");      
 	     
 	     System.out.println("Success loading Mysql Driver!");
 	    }
@@ -21,12 +21,12 @@ public class websites {
 	    try {
 	      Connection connect = (Connection) DriverManager.getConnection(
 	          "jdbc:mysql://localhost:3306/java","java","pass");
-	           //连接URL为   jdbc:mysql//服务器地址/数据库名  ，后面的2个参数分别是登陆用户名和密码
+	           
 
 	      System.out.println("Success connect Mysql server!");
 	      Statement stmt = (Statement) connect.createStatement();
 	      ResultSet rs = stmt.executeQuery("select * from websites");
-	                                                              //user 为你表的名称
+	                                                             
 	while (rs.next()) {
 	        System.out.println(rs.getString("name"));
 	      }
