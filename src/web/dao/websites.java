2170dbce04b9ad5675523d15239fad2ab8acf602
tan kinka <lunwu.xiaoxia@gmail.com>
2016-12-17 23:44:58 +0900

    web hello world.

---
+++ src/web/dao/websites.java
@@ -0,0 +1,39 @@
+package web.dao;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+public class websites {
+	
+	public static void main(String args[]) {
+	    try {
+	      Class.forName("com.mysql.jdbc.Driver");     //加载MYSQL JDBC驱动程序   
+	      //Class.forName("org.gjt.mm.mysql.Driver");
+	     System.out.println("Success loading Mysql Driver!");
+	    }
+	    catch (Exception e) {
+	      System.out.print("Error loading Mysql Driver!");
+	      e.printStackTrace();
+	    }
+	    try {
+	      Connection connect = (Connection) DriverManager.getConnection(
+	          "jdbc:mysql://localhost:3306/java","java","pass");
+	           //连接URL为   jdbc:mysql//服务器地址/数据库名  ，后面的2个参数分别是登陆用户名和密码
+
+	      System.out.println("Success connect Mysql server!");
+	      Statement stmt = (Statement) connect.createStatement();
+	      ResultSet rs = stmt.executeQuery("select * from websites");
+	                                                              //user 为你表的名称
+	while (rs.next()) {
+	        System.out.println(rs.getString("name"));
+	      }
+	    }
+	    catch (Exception e) {
+	      System.out.print("get data error!");
+	      e.printStackTrace();
+	    }
+	  }
+}
